c563fdcd45e3aeb4d767c5550da82d7ca579e7e8
madarkd <diogo.m.s.lopes@gmail.com>
2019-12-01 15:13:20 +0000

    video pop up on homepage

--- wp-content/themes/tmwf/sections/section-textvideo.php
+++ wp-content/themes/tmwf/sections/section-textvideo.php
@@ -1,5 +1,69 @@
+<?php if( is_front_page() ): ?>
 <section class="textvideo-section content-section <?php if(get_sub_field( 'content_side' )){echo 'right';}else{echo 'left';}; ?>">
+    <div class="container-large">
+        <?php if ( have_rows( 'group_left' ) ) : ?>
+            <?php while ( have_rows( 'group_left' ) ) : the_row(); ?>
+                <div class="col-7">
+                    <div class="video-wrapper">
+                        <?php $overlay = get_sub_field( 'overlay' ); ?>
+                        <?php if ( $overlay ) { ?>
+                        <div class="image-overlay">
+                            <a href="#">
+                            <?php echo file_get_contents(get_template_directory_uri() . '/img/icons/play.svg'); ?>
+                            </a>
+                            <img src="<?php echo $overlay['url']; ?>" alt="<?php echo $overlay['alt']; ?>" />
+                        </div>
+                        <?php } ?>
+                        <?php $videosrc = get_sub_field( 'video' ); ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        
+        <div class="col-5">
+            <?php if ( have_rows( 'group_right' ) ) : ?>
+                <?php while ( have_rows( 'group_right' ) ) : the_row(); ?>
+                    <div class="content-wrapper">
+                        <?php the_sub_field( 'content' ); ?>
+                        <?php if(get_sub_field( 'button_url' )): ?>
+                            <a href="<?php echo the_sub_field( 'button_url' ); ?>" class="arrow-link"><?php the_sub_field( 'button_text' ); ?></a>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
 
+<div class="modal" id="video">
+    <div class="modal__container">
+        <?php echo $videosrc; ?>
+    </div>
+</div>
+
+<script>
+(function($){
+    $('.image-overlay').on('click', 'a', function(e){
+        e.preventDefault();
+
+        $('#video').addClass('active');
+
+        //$(this).parent().fadeOut();
+        var videoURL = $('#video').prop('src');
+        videoURL += "&autoplay=1";
+        $(this).parent().next().prop('src',videoURL);
+    });
+
+    $('.modal').on('click', function(e){
+        e.preventDefault();
+        if (e.target == this) {
+            $('.modal').removeClass("active");
+        }
+    });
+})(jQuery);
+</script>
+<?php else: ?>
+<section class="textvideo-section content-section <?php if(get_sub_field( 'content_side' )){echo 'right';}else{echo 'left';}; ?>">
     <div class="container-large">
         <?php if ( have_rows( 'group_left' ) ) : ?>
             <?php while ( have_rows( 'group_left' ) ) : the_row(); ?>
@@ -36,14 +100,15 @@
 </section>
 
 <script>
-    (function($){
-        $('.image-overlay').on('click', 'a', function(e){
-            e.preventDefault();
+(function($){
+    $('.image-overlay').on('click', 'a', function(e){
+        e.preventDefault();
 
-            $(this).parent().fadeOut();
-            var videoURL = $(this).parent().next().prop('src');
-            videoURL += "&autoplay=1";
-            $(this).parent().next().prop('src',videoURL);
-        });
-    })(jQuery);
+        $(this).parent().fadeOut();
+        var videoURL = $(this).parent().next().prop('src');
+        videoURL += "&autoplay=1";
+        $(this).parent().next().prop('src',videoURL);
+    });
+})(jQuery);
 </script>
+<?php endif; ?>
