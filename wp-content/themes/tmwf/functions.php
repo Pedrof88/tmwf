edfafdc31d4213d4ce877f2f46c3a527f1eeb5c0
madarkd <diogo.m.s.lopes@gmail.com>
2019-12-31 01:48:40 +0000

    cenas

--- wp-content/themes/tmwf/functions.php
+++ wp-content/themes/tmwf/functions.php
@@ -434,8 +434,7 @@ function html5blankcomments($comment, $args, $depth)
     <?php endif; ?>
     <?php }
 
-
-// ACF first request
+    // ACF first request
 function acf_loadmore( $field, $ppp, $current, $template ) {
 
 	if( $field && $ppp ){
@@ -463,17 +462,7 @@ function acf_loadmore( $field, $ppp, $current, $template ) {
 
 			endforeach; endif;
 
-		}else {
-            echo '<script>var maxpage = '.$pages.'</script>';
-
-			$fieldArray = array_slice($queryField, $start, $last);
-
-			if( $fieldArray ) : foreach( $fieldArray as $item ) :
-		
-				include(locate_template('sections/'.$template.'.php'));
-
-			endforeach; endif;
-        }
+		}
 
 	}
 
@@ -804,5 +793,75 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();
 	
 }
+
+function new_excerpt_more($more) {
+global $post;
+remove_filter('excerpt_more', 'new_excerpt_more'); 
+return '';
+}
+add_filter('excerpt_more','new_excerpt_more',11);
+
+function tn_custom_excerpt_length( $length ) {
+return 15;
+}
+add_filter( 'excerpt_length', 'tn_custom_excerpt_length', 999 );
+
+function misha_my_load_more_scripts() {
+ 
+	global $wp_query; 
+ 
+	// In most cases it is already included on the page and this line can be removed
+	wp_enqueue_script('jquery');
+ 
+	// register our main script but do not enqueue it yet
+	//wp_register_script( 'my_loadmore', get_stylesheet_directory_uri() . '/myloadmore.js', array('jquery') );
+ 
+	// now the most interesting part
+	// we have to pass parameters to myloadmore.js script but we can get the parameters values only in PHP
+	// you can define variables directly in your HTML but I decided that the most proper way is wp_localize_script()
+	wp_localize_script( 'jquery', 'misha_loadmore_params', array(
+		'ajaxurl' => site_url() . '/wp-admin/admin-ajax.php', // WordPress AJAX
+		'posts' => json_encode( $wp_query->query_vars ), // everything about your loop is here
+		'current_page' => get_query_var( 'paged' ) ? get_query_var('paged') : 1,
+		'max_page' => $wp_query->max_num_pages
+	) );
+ 
+ 	wp_enqueue_script( 'my_loadmore' );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'misha_my_load_more_scripts' );
+
+function misha_loadmore_ajax_handler(){
+ 
+	// prepare our arguments for the query
+	$args = json_decode( stripslashes( $_POST['query'] ), true );
+	$args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
+	$args['post_status'] = 'publish';
+ 
+	// it is always better to use WP_Query but not here
+	query_posts( $args );
+ 
+	if( have_posts() ) :
+ 
+		// run the loop
+		while( have_posts() ): the_post();
+ 
+			// look into your theme code how the posts are inserted, but you can use your own HTML of course
+			// do you remember? - my example is adapted for Twenty Seventeen theme
+			get_template_part( 'sections/news-single');
+			// for the test purposes comment the line above and uncomment the below one
+			// the_title();
+ 
+ 
+		endwhile;
+ 
+	endif;
+	die; // here we exit the script and even no wp_reset_query() required!
+}
+ 
+ 
+ 
+add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
+add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
 
 ?>
