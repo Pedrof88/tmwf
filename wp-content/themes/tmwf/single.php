6d7edc6516426775090977867f079dfca960eb91
madarkd <diogo.m.s.lopes@gmail.com>
2020-01-04 21:29:44 +0000

    cometer as cenas como esta em live

--- wp-content/themes/tmwf/single.php
+++ wp-content/themes/tmwf/single.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 	<main role="main">
-        <!-- SECTIONS -->
-        <?php get_template_part("sections/core");?>
-        <!-- SECTIONS -->
+    <!-- SECTIONS -->
+    <?php get_template_part("sections/core");?>
+    <!-- SECTIONS -->
 	</main>
 
 <?php get_footer(); ?>
