6d71dd4370b438f3703aa06938b4d8e4e8dedfdd
unknown <pedroferreira.email@gmail.com>
2020-04-26 17:55:18 +0100

    form added to the leaderboard

--- wp-content/themes/tmwf/page.php
+++ wp-content/themes/tmwf/page.php
@@ -3,6 +3,17 @@
 	<main role="main">
     <!-- SECTIONS -->
     <?php get_template_part("sections/core");?>
+
+    <?php if (is_page(37)) { ?>
+        <section class="form">
+        <div class="container-large">
+            <div class="col-12">
+                <h2 class="center-aligned">Submit your walks data</h2>
+            </div>
+            <?php echo do_shortcode('[contact-form-7 id="1055" title="Leaderboard Form"]');?>
+        </div>
+    </section>
+    <?php }?>
     <!-- SECTIONS -->
 	</main>
 
